2fd882adcae59ec210ea1f20ad42a43f9ec301c2
Aarão Melo <aarao.melo.lopes@gmail.com>
2021-03-24 16:01:39 -0300

    prefix

--- src/store/modules/services/state.js
+++ src/store/modules/services/state.js
@@ -1,5 +1,5 @@
 const state = {
-  url: process.env.VUE_APP_BASE_URL || '',
+  url: `${process.env.VUE_APP_BASE_URL || ''}api/`,
   supersecret: process.env.VUE_APP_SUPER_SECRET_KEY || 'chavemuitosegura',
   token: localStorage.getItem('access_token') || '',
   response: {},
