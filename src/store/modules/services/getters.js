254e3bb6b76cef624b3839d6b90851cde85a94f9
Aarão Melo <aarao.melo.lopes@gmail.com>
2021-03-31 19:06:59 -0300

    default image

--- src/store/modules/services/getters.js
+++ src/store/modules/services/getters.js
@@ -4,7 +4,7 @@ const getters = {
   decodeToken: (state) => jwt.verify(state.token, state.supersecret),
   getToken: (state) => state.token,
   getImagePath: (state) => `${state.url}images/`,
-  getDefaultImage: (state) => `${state.url}images/eb1d9a0ef3c078316330e6a4af53b8a6.png`,
+  getDefaultImage: (state) => `${state.url}images/default.png`,
   getSuperSecretKey: (state) => state.supersecret,
   httpConfigs(state) {
     return {
