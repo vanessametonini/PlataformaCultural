1bb3660d0f15a2c8dd31f10225521602c3d3b1b8
Cintia <cin.contaadm@gmail.com>
2022-01-06 08:28:47 -0300

    Adição de mensagem ao usuário quando este ainda não tiver criado nenhum pin, evento ou debate #13

--- src/views/profile/Sliders.vue
+++ src/views/profile/Sliders.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="profile">
-    <section class="profile-carousel">
-      <h2>Meus Pins</h2>
-      <pins-profile />
-    </section>
-    <section class="profile-carousel">
-      <h2>Meus Debates</h2>
-      <topics-profile />
-    </section>
-    <section class="profile-carousel">
-      <h2>Meus Eventos</h2>
-      <events-profile />
-    </section>
+    <template v-if="pins || events || topics">
+      <section class="profile-carousel">
+        <h2>Meus Pins</h2>
+        <pins-profile />
+      </section>
+      <section class="profile-carousel">
+        <h2>Meus Debates</h2>
+        <topics-profile />
+      </section>
+      <section class="profile-carousel">
+        <h2>Meus Eventos</h2>
+        <events-profile />
+      </section>
+    </template>
+    <template v-else>
+      <h2 class="empty">Faça sua primeira colaboração, insira seu pin, um evento ou debate.</h2>
+    </template>
   </div>
 </template>
 <script>
@@ -31,46 +36,68 @@ export default {
       topicSlider: 1,
       eventSlider: 1,
     }
+  },
+  computed: {
+   pins() {
+     return this.$store.getters['pins/getMyPins'].length > 0;
+   },
+   events() {
+     return this.$store.getters['events/getMyEvents'].length > 0;
+   },
+   topics() {
+     return this.$store.getters['topics/getMyTopics'].length > 0;
+   }
+
   }
 }
 </script>
 <style lang="scss" scoped>
 .profile {
-   padding: 16px;
-}
+   padding: 32px;
 
-.profile-carousel {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-bottom: 40px;
+  .profile-carousel {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin-bottom: 40px;
 
-  h2 {
-    font-size: 2em;
-    font-weight: bolder;
-    margin: 0 0 10px;
-    position: relative;
-    align-self: flex-start;
+    h2 {
+      font-size: 2em;
+      font-weight: bolder;
+      margin: 0 0 10px;
+      position: relative;
+      align-self: flex-start;
+
+    }
+
+    .btn-add {
+      background: black;
+      bottom: -40px;
+      color: white;
+      content: "+";
+      cursor: pointer;
+      display: block;
+      font-size: 1em;
+      font-weight: bolder;
+      height: 50px;
+      right: -10px;
+      line-height: 50px;
+      position: absolute;
+      text-align: center;
+      text-decoration: none;
+      width: 50px;
+      z-index: 1;
+    }
+
+
   }
+  .empty {
+    font-size: 1.8rem;
+    font-weight: bold;
 
-  .btn-add {
-    background: black;
-    bottom: -40px;
-    color: white;
-    content: "+";
-    cursor: pointer;
-    display: block;
-    font-size: 1em;
-    font-weight: bolder;
-    height: 50px;
-    right: -10px;
-    line-height: 50px;
-    position: absolute;
-    text-align: center;
-    text-decoration: none;
-    width: 50px;
-    z-index: 1;
+    @media screen and (min-width: 1024px) {
+      max-width: 360px;      
+    }
   }
-
 }
 </style>
